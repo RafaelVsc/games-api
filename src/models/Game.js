2fe21a951b66e64d96e445ed62128500ec144cb7
RafaelVsc <rafaelvsc88@gmail.com>
2025-05-29 21:33:10 -0300

    "add some mongoose custom validators"

--- src/models/Game.js
+++ src/models/Game.js
@@ -14,8 +14,31 @@ const gameSchema = new mongoose.Schema(
         message: "At least one platform must be specified",
       },
     },
-    releaseYear: { type: Number, required: false, default: null },
-    durationHours: { type: Number },
+    releaseYear: {
+      type: Number,
+      min: [1960, "Release year must be 1960 or later"],
+      max: [
+        new Date().getFullYear(),
+        "Release year cannot be in the far future",
+      ],
+      default: null,
+      validate: {
+        validator: (value) => value === null || Number.isInteger(value),
+        message: "Release year must be an integer",
+      },
+      required: false,
+    },
+    durationHours: {
+      type: Number,
+      // min: [1, "Minimum duration must be at least 1 hour"],
+      // max: [100, "Maximum duration cannot exceed 100 hours"],
+      validate: {
+        validator: (value) => {
+          return value >= 1 && value <= 100;
+        },
+        message: "Duration hours must be between 1 and 100 hours",
+      },
+    },
     developer: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Developer",
